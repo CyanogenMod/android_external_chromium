2a3fa35dc168f711f2d6a07c66614f236b2202f3
Iain Merrick <husky@google.com>
2010-11-04 11:41:25 +0000

    Temporarily disable X509 certificate verification.
    Broken in recent Chromium merge (https://android-git.corp.google.com/g/77650).
    We use the same workaround as Chrome: log a warning, ignore the error. We're
    still pulling changes from Chrome so we expect this to be fixed soon.
    
    Change-Id: I8e6d8aa5ccb341d156a9623841200e631816b2a6

--- net/base/x509_certificate_openssl.cc
+++ net/base/x509_certificate_openssl.cc
@@ -456,7 +456,12 @@ int X509Certificate::Verify(const std::string& hostname,
   if (X509_verify_cert(ctx.get()) == 1) {
     return OK;
   }
-
+#if defined(ANDROID)
+  // TODO: Remove this hack when the trusted CA root certificates are imported
+  // from the Java side.
+  LOG(WARNING) << "X509 certificate not verified (NOT IMPLEMENTED YET)";
+  return OK;
+#else
   int x509_error = X509_STORE_CTX_get_error(ctx.get());
   int cert_status = MapCertErrorToCertStatus(x509_error);
   LOG(ERROR) << "X509 Verification error "
@@ -466,6 +471,7 @@ int X509Certificate::Verify(const std::string& hostname,
       << " : " << cert_status;
   verify_result->cert_status |= cert_status;
   return MapCertStatusToNetError(verify_result->cert_status);
+#endif
 }
 
 // static
