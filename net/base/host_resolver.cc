dca1fda5df8a49398dc98c4f6e4a0a1505662dc2
Ben Murdoch <benm@google.com>
2010-11-18 19:33:12 +0000

    Merge Chromium at r65505: Update CreateSystemHostResolver calls
    Need to add a new parameter to this function to set a resolver_proc.
    NULL means that we use getaddrinfo(), which seems safe.
    
    http://src.chromium.org/viewvc/chrome?view=rev&revision=64271
    
    Change-Id: Id0c8afade30656210aa1177334e1e5f52b849548

--- net/base/host_resolver.cc
+++ net/base/host_resolver.cc
@@ -51,7 +51,7 @@ int SingleRequestHostResolver::Resolve(const HostResolver::RequestInfo& info,
   CompletionCallback* transient_callback = callback ? &callback_ : NULL;
 
 #ifdef ANDROID
-  HostResolver* hr = CreateSystemHostResolver(HostResolver::kDefaultParallelism, NULL);
+  HostResolver* hr = CreateSystemHostResolver(HostResolver::kDefaultParallelism, NULL, NULL);
   int rv = hr->Resolve(info, addresses, transient_callback, &request, net_log);
 #else
   int rv = resolver_->Resolve(
