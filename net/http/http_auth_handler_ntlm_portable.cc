7dd039766e8bea164f5e14cb9ab26c59d3b9a5bd
Paul Lind <plind@mips.com>
2012-08-03 08:54:28 -0700

    Add MIPS architecture support
    Change-Id: Id2c3f41edee0e1c73cb084666533802bcf117876
    Signed-off-by: Douglas Leung <douglas@mips.com>
    Signed-off-by: Chris Dearman <chris@mips.com>

--- net/http/http_auth_handler_ntlm_portable.cc
+++ net/http/http_auth_handler_ntlm_portable.cc
@@ -70,9 +70,13 @@ namespace net {
  * ***** END LICENSE BLOCK ***** */
 
 // Discover the endianness by testing processor architecture.
-#if defined(ARCH_CPU_X86) || defined(ARCH_CPU_X86_64) || defined(ARCH_CPU_ARMEL)
+#if defined(ARCH_CPU_X86) || defined(ARCH_CPU_X86_64)\
+ || defined(ARCH_CPU_ARMEL) || defined(ARCH_CPU_MIPSEL)
 #define IS_LITTLE_ENDIAN 1
 #undef  IS_BIG_ENDIAN
+#elif defined(ARCH_CPU_MIPSEB)
+#undef IS_LITTLE_ENDIAN
+#define  IS_BIG_ENDIAN 1
 #else
 #error "Unknown endianness"
 #endif
